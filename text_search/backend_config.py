b05e3229d792b60c9d7919f472f3cadbd618d6fb
Alex C-G <alexcg@outlook.com>
2021-06-18 13:13:55 +0200

    text-backend: port change

--- text_search/backend_config.py
+++ text_search/backend_config.py
@@ -5,7 +5,7 @@ backend_model = "sentence-transformers/paraphrase-distilroberta-base-v1"
 backend_top_k = 10
 
 # in app.py
-backend_port = 45678
+backend_port = 45679
 backend_workdir = "workspace"
 backend_datafile = "../../data/memes.json"
-primary_field = "caption_text" # Primary field in data file (this will be assigned to Document.text and indexed. Other fields are metadata only)
+max_docs = 5000
