9287e9bbde3ad596b23d73fb296774e6f234f86d
Alex C-G <alexcg@outlook.com>
2021-09-06 12:47:57 +0200

    fix: data url

--- get_data.sh
+++ get_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p data
-wget -O data/memes.json https://github.com/alexcg1/ml-datasets/blob/master/nlp/memes/memes.json?raw=true
+wget -O data/memes.json https://jina-examples-datasets.s3.amazonaws.com/memes/memes.json
