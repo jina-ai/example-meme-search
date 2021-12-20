025147d32884149324c23a53a66df8c35da8dd3d
Alex C-G <alexcg@outlook.com>
2021-12-20 12:27:54 +0100

    fix: better sample strings

--- frontend/config.py
+++ frontend/config.py
@@ -1,6 +1,7 @@
 # Text search
 text_endpoint = "http://examples.jina.ai:45678/search"
 text_endpoint = "http://0.0.0.0:45679/search"
+samples = ["cute fuzzy animals", "so you're telling me willy wonka", "school sucks"]
 
 # Image search
 image_endpoint = "http://examples.jina.ai:12345/search"
@@ -11,3 +12,4 @@ image_size = 128 # How big to render images
 # General
 top_k = 10
 DEBUG = False
+
