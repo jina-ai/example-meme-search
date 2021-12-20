3fa81620a975d577e76f8fb91a353cc59e0e15c8
Alex C-G <alexcg@outlook.com>
2021-12-20 14:48:59 +0100

    chore: cleanup

--- backend-image/executors.py
+++ backend-image/executors.py
@@ -1,14 +1,5 @@
 from jina import Executor, requests, Document, DocumentArray
-import os
 
-
-class UriToBlob(Executor):
-    @requests
-    def uri_to_blob(self, docs, **kwargs):
-        for doc in docs:
-            doc.tags["uri"] = doc.uri
-            doc.tags["uri_absolute"] = os.path.abspath(doc.uri)
-            doc.convert_image_uri_to_blob()
 
 class ImageNormalizer(Executor):
     @requests
