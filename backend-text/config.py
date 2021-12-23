0b4eabc90f270b1d47aa0e0e22f38900f3faee96
Alex C-G <alexcg@outlook.com>
2021-12-23 12:52:50 +0000

    fix: file path

--- backend-text/config.py
+++ backend-text/config.py
@@ -1,6 +1,6 @@
 model = "en_core_web_md" # Which SpaCy model do we use?
 port = 45679
 WORKSPACE_DIR = "workspace"
-datafile = "./data/memes.json"
+datafile = "../data/memes.json"
 max_docs = 1000
 random_seed = 1337
